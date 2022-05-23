be0fd2b96ac4be6401726b977a3f368dfa4bc25a
Laing91 <86000107+Laing91@users.noreply.github.com>
2022-05-23 23:55:02 +0100

    added responsive navbar

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "main-nav") {
-      x.className += " responsive";
+function toggleNavbar() {
+    var x = document.querySelector(".main-nav");
+    if (x.classList.contains("show")) {
+      x.classList.remove("show");
     } else {
-      x.className = "main-nav";
+      window.addEventListener("click", hideNav)
+      x.classList.add("show");
     }
   }
+
+function hideNav(e) {
+    let button = document.querySelector("#hamburgerButton")
+    if (e.target == button) {
+        return
+    }
+    var x = document.querySelector(".main-nav");
+    x.classList.remove("show");
+    window.removeEventListener("click", hideNav)
+}
